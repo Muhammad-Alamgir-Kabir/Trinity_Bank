4089018428c0a1366bb049df72ec8aedcca434ae
Alamgir Kabir <alamgirkabirbablu689@gmail.com>
2025-10-11 19:38:43 +0600

    Trinity Bank Codes

--- Bank_Management/settings.py
+++ Bank_Management/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = 'django-insecure-4977vk$+nypnim(dka-#94u2p@z5%4)y%^c5ttxh$=fawr%vs#'
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
